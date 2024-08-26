eb32cbedb163afbc3cb2b71f4eab7c3ed8e63bbf
Farditt <farhandito15@gmail.com>
2024-08-26 15:24:55 +0700

    fixing room page

--- resources/views/landing/layouts/room.blade.php
+++ resources/views/landing/layouts/room.blade.php
@@ -1,17 +1,17 @@
-<div class="container-xxl py-5">
+
+<div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+    <h6 class="section-title text-center text-primary text-uppercase">Our Services</h6>
+    <h1 class="mb-5">Explore Our <span class="text-primary text-uppercase">Services</span></h1>
+</div>
+<div class="container-xxl testimonial my-5 py-5 bg-dark wow zoomIn" data-wow-delay="0.1s">
     <div class="container">
-        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h6 class="section-title text-center text-primary text-uppercase">Our Rooms</h6>
-            <h1 class="mb-5">Explore Our <span class="text-primary text-uppercase">Rooms</span></h1>
-        </div>
-        <div class="row g-4">
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="owl-carousel testimonial-carousel py-5">
                 <div class="room-item shadow rounded overflow-hidden">
                     <div class="position-relative">
                         <img class="img-fluid" src="{{ asset('hotelier-1.0.1') }}/img/room-1.jpg" alt="">
-                        <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">$100/Night</small>
+                        <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">$1</small>
                     </div>
-                    <div class="p-4 mt-2">
+                    <div class="p-4 mt-2 bg-white rounded overflow-hidden">
                         <div class="d-flex justify-content-between mb-3">
                             <h5 class="mb-0">Junior Suite</h5>
                             <div class="ps-2">
@@ -34,16 +34,14 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                 <div class="room-item shadow rounded overflow-hidden">
                     <div class="position-relative">
                         <img class="img-fluid" src="{{ asset('hotelier-1.0.1') }}/img/room-2.jpg" alt="">
                         <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">$100/Night</small>
                     </div>
-                    <div class="p-4 mt-2">
+                    <div class="p-4 mt-2 bg-white rounded overflow-hidden">
                         <div class="d-flex justify-content-between mb-3">
-                            <h5 class="mb-0">Executive Suite</h5>
+                            <h5 class="mb-0">Junior Suite</h5>
                             <div class="ps-2">
                                 <small class="fa fa-star text-primary"></small>
                                 <small class="fa fa-star text-primary"></small>
@@ -64,16 +62,14 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.6s">
                 <div class="room-item shadow rounded overflow-hidden">
                     <div class="position-relative">
                         <img class="img-fluid" src="{{ asset('hotelier-1.0.1') }}/img/room-3.jpg" alt="">
                         <small class="position-absolute start-0 top-100 translate-middle-y bg-primary text-white rounded py-1 px-3 ms-4">$100/Night</small>
                     </div>
-                    <div class="p-4 mt-2">
+                    <div class="p-4 mt-2 bg-white rounded overflow-hidden">
                         <div class="d-flex justify-content-between mb-3">
-                            <h5 class="mb-0">Super Deluxe</h5>
+                            <h5 class="mb-0">Junior Suite</h5>
                             <div class="ps-2">
                                 <small class="fa fa-star text-primary"></small>
                                 <small class="fa fa-star text-primary"></small>
@@ -94,7 +90,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
 </div>
